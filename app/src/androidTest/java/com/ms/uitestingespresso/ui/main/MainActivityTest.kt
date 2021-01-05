6406809f82293ad619139884f74e246431ffccdd
Sarik111 <gakk.hasan@gmail.com>
2021-01-05 12:39:54 +0600

    Navigation activity test

--- app/src/androidTest/java/com/ms/uitestingespresso/ui/main/MainActivityTest.kt
+++ app/src/androidTest/java/com/ms/uitestingespresso/ui/main/MainActivityTest.kt
@@ -2,6 +2,8 @@ package com.codingwithmitch.espressouitestexamples.ui.main
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
@@ -18,24 +20,24 @@ import org.junit.runner.RunWith
 class MainActivityTest {
 
     @Test
-    fun test_isActvityInView() {
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+    fun test_navSecondaryActivity() {
+        ActivityScenario.launch(MainActivity::class.java)
+
+        onView(withId(R.id.button_next_activity)).perform(click())
+        onView(withId(R.id.secondary)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_backPressNavMainActivity() {
+        ActivityScenario.launch(MainActivity::class.java)
+
+        onView(withId(R.id.button_next_activity)).perform(click())
+        onView(withId(R.id.secondary)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.button_back)).perform(click()) // method 1
+        //pressBack() // method 2
 
         onView(withId(R.id.main)).check(matches(isDisplayed()))
-    }
-
-    @Test
-    fun testVisiblity_TitleAndButtonIsVisible() {
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-
-        onView(withId(R.id.activity_main_title)).check(matches(isDisplayed())) // method 1
-        onView(withId(R.id.button_next_activity)).check(matches(withEffectiveVisibility(Visibility.VISIBLE))) //method 2
-    }
-
-    @Test
-    fun setTitle_checkTextVisibility() {
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-        onView(withId(R.id.activity_main_title)).check(matches(withText(R.string.text_mainactivity)))
 
     }
 }
